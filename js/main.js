964cf3a6e16299a9e5db93fdf30901d2a77b5b53
Sora Arakawa <sr@arkw.jp>
2020-07-23 16:28:36 +0900

    Rev 14 : Optimization, Add Toolchip

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 /*
 静岡大学 バーチャル情報学部
-Prototype / Rev.13
+Prototype / Rev.14
 
 (c)2020 Shizuoka University all rights reserved.
 Developed by Shizuoka University xR Association "Hamaria"
@@ -8,11 +8,8 @@ Developed by Shizuoka University xR Association "Hamaria"
 
 import { VRButton } from './WebVR.js';
 
-var controls;
 var html = "";
-var mouse = { x: 0, y: 0 };
-var targetList = [];
-var renderer, scene, camera, controls, material, mesh;
+var renderer, scene, camera, controls;
 var chip = 0;
 var chip_tx = "";
 var chip_id = "";
@@ -21,6 +18,7 @@ var dy = 0;
 var fade = 0;
 var walkthrough = null;
 var prevTime, curTime;
+var clicker = {prev:{}, next: {}}
 var player = {
 	speed: 7.0,
 	controller: null,
@@ -30,12 +28,53 @@ var player = {
 	getControllerIndex : function() {
 		return 0;
 	},
-	getHorizontal : function(pad) {	//must return -1.0 ~ 1.0
+	getHorizontal : function(pad) {	// must return -1.0 ~ 1.0
 		return -pad.axes[3];
 	},
-	getVertical : function(pad) {	//must return -1.0 ~ 1.0
+	getVertical : function(pad) {	// must return -1.0 ~ 1.0
 		return pad.axes[2];
 	}
+};
+
+const domChip = $("#chip");
+const domCover = $("#cover");
+const domDebug = $("#debug_camera");
+const domDialog = $("#dialog");
+
+var parentMap = {};
+const ChipBase = function(id, label, impl = false) {
+	this.id = id;
+	this.label = label;
+	this.impl = impl;
+};
+const toolChips = {
+	inf_1: new ChipBase("inf_1", "情報学部1号館"),
+	inf_2: new ChipBase("inf_2", "情報学部2号館"),
+	s_port: new ChipBase("s-port", "S-Port", true),
+	innovation: new ChipBase("innovation", "イノベーション社会連携推進機構"),
+	south_hall: new ChipBase("south_hall", "南会館"),
+	sanaru_hall: new ChipBase("sanaru_hall", "佐鳴会館"),
+	kagai: new ChipBase("kagai", "課外活動共同施設"),
+	takayanagi: new ChipBase("takayanagi", "高柳記念未来技術創造館"),
+	budo: new ChipBase("budo", "武道場"),
+	gym: new ChipBase("gym", "体育館"),
+	north_hall: new ChipBase("north_hall", "北会館"),
+	eng_8: new ChipBase("eng_8", "工学部8号館"),
+	eng_7: new ChipBase("eng_7", "工学部7号館"),
+	eng_4: new ChipBase("eng_4", "工学部4号館"),
+	eng_5: new ChipBase("eng_5", "工学部5号館"),
+	monozukuri_house: new ChipBase("monozukuri_house", "ものづくり館"),
+	monozukuri_center: new ChipBase("monozukuri_center", "ものづくりセンター"),
+	eng_1: new ChipBase("eng_1", "工学部1号館"),
+	eng_2: new ChipBase("eng_2", "工学部2号館"),
+	eng_6: new ChipBase("eng_6", "工学部6号館"),
+	eng_3: new ChipBase("eng_3", "工学部3号館"),
+	nanodevice: new ChipBase("nanodevice", "ナノデバイス"),
+	electronics: new ChipBase("electronics", "電子工学研究拠点"),
+	hikari_soki: new ChipBase("hikari_soki", "光創起イノベーション研究拠点"),
+	inf_graduate: new ChipBase("inf_graduate", "創造科学技術大学院"),
+	lecture_building: new ChipBase("lecture_building", "共通講義棟"),
+	sogo: new ChipBase("sogo", "総合研究棟"),
 };
 
 window.addEventListener('DOMContentLoaded', init);
@@ -44,24 +83,44 @@ $(window).on('touchmove.noScroll', function(e) {
 	e.preventDefault();
 });
 
-document.body.onclick = function() {
-	if(chip == 1) {
-		$("#dialog_title").text(chip_tx);
-		$("#cover").css("display", "block");
-		$("#cover").css("opacity",0.3);
-		$("#dialog").show(500);
-		if(chip_tx == "S-Port") {
-			$("#dialog_main").load("contents/s-port.html");
-		}
-		fade = 0;
-		dialog = 1;
+document.body.addEventListener('mousedown', (evt) => {
+	clicker.prev = {x:evt.screenX, y:evt.screenY};
+});
+document.body.addEventListener('touchstart', (evt) => {
+	clicker.prev = {x:evt.screenX, y:evt.screenY};
+});
+document.body.addEventListener('touchend', (evt) => {
+	let d = (evt.screenX - clicker.prev.x) * (evt.screenX - clicker.prev.x) + (evt.screenY - clicker.prev.y) * (evt.screenY - clicker.prev.y);
+	if(d <= 16) {
+		bodyClick();
 	}
+});
+document.body.addEventListener('mouseup', (evt) => {
+	let d = (evt.screenX - clicker.prev.x) * (evt.screenX - clicker.prev.x) + (evt.screenY - clicker.prev.y) * (evt.screenY - clicker.prev.y);
+	if(d <= 16) {
+		bodyClick();
+	}
+});
+
+function bodyClick() {
+	if(chip == 0)
+		return;
+	$("#dialog_title").text(chip_tx);
+	$("#cover").css("display", "block").css("opacity",0.3);;
+	domDialog.show(500);
+	if(toolChips[chip_id] && toolChips[chip_id].impl) {
+		$("#dialog_main").load(`contents/${toolChips[chip_id].id}.html`);
+	} else {
+		$("#dialog_main").html('');
+	}
+	fade = 0;
+	dialog = 1;
 }
 
 function init() {
 	// レンダラーを作成
 	renderer = new THREE.WebGLRenderer({
-		canvas: document.querySelector('#canvas'),
+		canvas: document.getElementById('canvas'),
 		antialias: true
 	});
 	var width = window.innerWidth;
@@ -73,7 +132,6 @@ function init() {
 	// VRボタンの有効をチェック後有効化
 	if(VRButton.enableVR()) {
 		renderer.xr.enabled = true;
-		document.body.appendChild(VRButton.createButton(renderer));
 	}
 
 	// シーンを作成
@@ -82,6 +140,7 @@ function init() {
 	// カメラを作成
 	camera = new THREE.PerspectiveCamera(45, width / height, 0.1, 1000);
 	controls = new THREE.OrbitControls(camera);
+	controls.maxDistance = 600;
 
 	const loader = new THREE.GLTFLoader();
 
@@ -94,10 +153,10 @@ function init() {
 	}
 
 	// 移動関連のコンポーネント初期化
-	walkthrough = new THREE.PointerLockControls(camera, document.querySelector('#canvas') );
+	walkthrough = new THREE.PointerLockControls(camera, document.getElementById('canvas') );
 	walkthrough.addEventListener('lock', () => {
 		player.birdPos = new THREE.Vector3(camera.position.x, camera.position.y, camera.position.z);
-		camera.position.set(116, 1.2, -50);
+		camera.position.set(116, 1.4, -50);
 	});
 	walkthrough.addEventListener('unlock', () => {
 		camera.matrixWorld = player.birdMatrix;
@@ -110,10 +169,11 @@ function init() {
 	});
 	window.addEventListener('enter_vr', () => {
 		if(camera != vrCamera)
-			vrCamera.position.set(116, 1.2, -50);
+			vrCamera.position.set(116, 1.4, -50);
 	});
-
-
+	window.addEventListener('exit_vr', () => {
+		vrCamera.position.set(0, 0, 0);
+	});
 	// 全体モデル
 	var model = null;
 	loader.load(
@@ -131,6 +191,19 @@ function init() {
 					child.material.polygonOffset = true;
 					child.material.polygonOffsetFactor = 1;
 					child.material.polygonOffsetUnits = -1;
+				}
+			}
+
+			let targets = [...gltf.scene.children];
+			while(targets.length > 0) {
+				let child = targets.pop();
+				for(let cc of child.children) {
+					targets.push(cc);
+				}
+				if(child.name) {
+					let parent = child;
+					while(parent.parent && parent.parent != model && (parent = parent.parent));
+					parentMap[child.name] = parent.name;
 				}
 			}
 			$("#cover").css("opacity",0);
@@ -186,7 +259,10 @@ function init() {
 	document.addEventListener( 'keydown', (evt) => keyCheck(evt, true), false );
 	document.addEventListener( 'keyup', (evt) => keyCheck(evt, false), false );
 
-	// 移動処理
+	function isFirstPersonMode() {
+		return walkthrough.isLocked || renderer.xr.isPresenting;
+	}
+
 	function tickMove() {
 		const delta = (curTime - prevTime) / 1000.0;
 		if(renderer.xr.isPresenting && player.controller) {
@@ -212,6 +288,7 @@ function init() {
 		
 	}
 
+
 	function tick() {
 		curTime = performance.now();
 		/*
@@ -220,7 +297,7 @@ function init() {
 		}
 		*/
 
-		if(!walkthrough.isLocked && !renderer.xr.isPresenting) {
+		if(!isFirstPersonMode()) {
 			controls.update();
 		}
 
@@ -230,36 +307,42 @@ function init() {
 		
 		// デバッグ用情報の表示
 		html = "[Camera Parameter]<br>X Position："+camera.position.x+"<br>Y Position："+camera.position.y+"<br>Z Position："+camera.position.z+"<br>X Rotation："+camera.rotation.x+"<br>Y Rotation："+camera.rotation.y+"<br>Z Rotation："+camera.rotation.z+"<br>X Scale："+camera.scale.x+"<br>Y Scale："+camera.scale.y+"<br>Z Scale："+camera.scale.z;
-		$("#debug_camera").html(html);
+		domDebug.html(html);
 		
 		// フェード処理
-		if (fade == 0 && $("#cover").css("opacity") <= 0) {
-			$("#cover").css("display", "none");
+		if (fade == 0 && domCover.css("opacity") <= 0) {
+			domCover.css("display", "none");
 			fade = 1;
+			document.body.appendChild(VRButton.createButton(renderer));
 		}
 		
 		// ツールチップ処理
-		if (chip == 0) {
-			$("#chip").hide();
-		}
-		if (chip == 1) {
-			$("#chip").show();
-		}
-		
-		if (dialog == 1) {
-			if (dy != $("#dialog").height()) {
-				dy = $("#dialog").height();
-				$("#dialog_main").height(dy - 62);
+		if(!isFirstPersonMode()) {
+			if (chip == 0) {
+				domChip.hide();
 			}
-		}
+			if (chip == 1) {
+				domChip.show();
+			}
+			domChip.text(chip_tx);
 		
-		$("#chip").text(chip_tx);
+			if (dialog == 1) {
+				if (dy != domDialog.height()) {
+					dy = domDialog.height();
+					$("#dialog_main").height(dy - 62);
+				}
+			}
+		
+			domChip.text(chip_tx);
+		}
 		prevTime = curTime;
 	}
 }
 
 window.onmousemove = function (ev){
-	var hit = 0;
+	if(walkthrough && walkthrough.isLocked)
+		return;
+	var hit = false;
 	
 	// 画面上のマウスクリック位置
 	var x = event.clientX;
@@ -277,30 +360,19 @@ window.onmousemove = function (ev){
 	// オブジェクトの取得
 	var intersects = raycaster.intersectObjects(scene.children, true);
 	for (var i = 0; i < intersects.length; i++) {
-		if (intersects[i].object.name.indexOf("Plane.002") != -1) {
+		const parent = parentMap[intersects[i].object.name];
+		if(parent && toolChips[parent]) {
+			domChip.css("left", x).css("top", y);
+			chip_id = parent;
+			chip_tx = toolChips[parent].label;
 			chip = 1;
-			chip_tx = "情報学部2号館";
-			chip_id = "Plane.002";
-		}
-		if (intersects[i].object.name.indexOf("map_5.osm_buildings.005") != -1) {
-			chip = 1;
-			chip_tx = "共通講義棟";
-			chip_id = "map_5.osm_buildings.005";
-		}
-		if (intersects[i].object.name.indexOf("map_5.osm_buildings.030") != -1) {
-			chip = 1;
-			chip_tx = "S-Port";
-			chip_id = "map_5.osm_buildings.030";
-		}
-		if (chip == 1) {
-			$("#chip").css("left", x);
-			$("#chip").css("top", y);
-		}
-		if (intersects[i].object.name.indexOf(chip_id) != -1) {
-			hit++;
+			hit = true;
+			break;
 		}
 	}
-	if (hit == 0){ chip = 0; }
+	if (!hit) { 
+		chip = 0;
+	}
 }
 
 // VRモードへ切り替え
